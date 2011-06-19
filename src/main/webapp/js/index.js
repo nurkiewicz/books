8ea88c225ab5c321390e186211bd86f0f2618b0f
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2011-06-19 19:41:37 +0200

    Default handling of AJAX form submitting, not REST at all

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -23,14 +23,70 @@ $(function() {
 				}
 			});
 
+	var URL = 'rest/book';
 	var options = {
-		url:'rest/book',
+		url: URL,
+		editurl: URL,
 		colModel:[
-			{name:'id', label: 'ID', formatter:'integer', width: 40},
-			{name:'title', label: 'Title', width: 300},
-			{name:'author', label: 'Author', width: 200},
-			{name:'publishedYear', label: 'Published year', width: 80, align: 'center'},
-			{name:'available', label: 'Available', formatter: 'checkbox', width: 46, align: 'center'}
+			{
+				name:'id', label: 'ID',
+				formatter:'integer',
+				width: 40,
+				editable: true,
+				editoptions: {disabled: true, size:5}
+			},
+			{
+				name:'title',
+				label: 'Title',
+				width: 300,
+				editable: true,
+				editrules: {required: true}
+			},
+			{
+				name:'author',
+				label: 'Author',
+				width: 200,
+				editable: true,
+				editrules: {required: true}
+			},
+			{
+				name:'cover',
+				label: 'Cover',
+				hidden: true,
+				editable: true,
+				edittype: 'select',
+				editrules: {edithidden:true},
+				editoptions: {
+					value: {'PAPERBACK': 'paperback', 'HARDCOVER': 'hardcover', 'DUST_JACKET': 'dust jacket'}
+				}
+			},
+			{
+				name:'publishedYear',
+				label: 'Published year',
+				width: 80,
+				align: 'center',
+				editable: true,
+				editrules: {required: true, integer: true},
+				editoptions: {size:5, maxlength: 4}
+			},
+			{
+				name:'available',
+				label: 'Available',
+				formatter: 'checkbox',
+				width: 46,
+				align: 'center',
+				editable: true,
+				edittype: 'checkbox',
+				editoptions: {value:"true:false"}
+			},
+			{
+				name:'comments',
+				label: 'Comments',
+				hidden: true,
+				editable: true,
+				edittype: 'textarea',
+				editrules: {edithidden:true}
+			}
 		],
 		caption: "Books",
 		pager : '#pager',
@@ -38,6 +94,6 @@ $(function() {
 	};
 	$("#grid")
 			.jqGrid(options)
-			.navGrid('#pager', {edit:false,add:false,del:false, search: false});
+			.navGrid('#pager', {edit:true,add:true,del:true, search: false});
 
 });
