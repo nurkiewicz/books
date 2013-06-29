abb14bb2738391ff0746841782f98150cb5b418f
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2013-06-29 21:46:56 +0200

    Inserting user name in every log statement

---
+++ src/main/java/com/blogspot/nurkiewicz/rest/UserNameFilter.java
@@ -0,0 +1,48 @@
+package com.blogspot.nurkiewicz.rest;
+
+import org.slf4j.MDC;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.switchuser.SwitchUserGrantedAuthority;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author Tomasz Nurkiewicz
+ * @since 6/29/13, 8:52 PM
+ */
+public class UserNameFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final String userName = authentication.getName();
+		final String realName = findSwitchedUser(authentication);
+
+		MDC.put("user", userName + (realName != null ? " (" + realName + ")" : ""));
+		try {
+			chain.doFilter(request, response);
+		} finally {
+			MDC.remove("user");
+		}
+	}
+
+	private String findSwitchedUser(Authentication authentication) {
+		for (GrantedAuthority auth : authentication.getAuthorities()) {
+			if (auth instanceof SwitchUserGrantedAuthority) {
+				return ((SwitchUserGrantedAuthority)auth).getSource().getName();
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
